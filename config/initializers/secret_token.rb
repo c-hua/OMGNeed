eae6586cebdb55c4ca7c90d277e2bd1f9d66b1b5
Eric Wu <ericwu@Erics-MacBook-Pro.local>
2014-05-28 16:21:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omgneed::Application.config.secret_key_base = '4222bc7d6e8fa8e340c1c7f0a875bbf7b907c573f7ea4e4fa1c479c1606f63a1690f03c7d8191e00de049e6fefd68a92f16038bb6a158f6337cc49e70767bc58'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Omgneed::Application.config.session_store :cookie_store, key: '_omgneed_session'
