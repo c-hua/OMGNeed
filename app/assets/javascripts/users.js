1e3f8c2a97b7c7f80da0d0538f48a200971ce65a
Eric Wu <ericwu@Erics-MacBook-Pro.local>
2014-06-04 19:40:52 -0700

    angular view with data

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,15 +1,4 @@
-
-
-
- // $(function(){ $(document).foundation(); });
-
- // $(function() {
- //    $( ".off-canvas-list" ).sortable();
- //    $( ".off-canvas-list" ).disableSelection();
- //  });
-
-Products = angular.module('product-app', ['ngResource', 'mm.foundation']).config(
-
+var omgneedApp = angular.module('omgneed-app', ['ngResource']).config(
     ['$httpProvider', function($httpProvider) {
     var authToken = angular.element("meta[name=\"csrf-token\"]").attr("content");
     var defaults = $httpProvider.defaults.headers;
@@ -20,159 +9,20 @@ Products = angular.module('product-app', ['ngResource', 'mm.foundation']).config
     defaults.common['Accept'] = 'application/json';
 }]);
 
-
-productApp.factory('User', ['$resource', function($resource) {
-  return $resource('/users/:id',
-
-Products.factory('Product', ['$resource', function($resource) {
-  return $resource('/products/:id',
-master
+omgneedApp.factory('User', ['$resource', function($resource) {
+  return $resource('/users/:id/',
      {id: '@id'},
      {update: { method: 'PATCH'}});
 }]);
 
+omgneedApp.controller('UserCtrl', ['$scope', 'User', function($scope, User) {
+
+  $scope.users= [];
+
+User.query(function(users) {
+      $scope.users = users;
+   });
 
 
 
-    // $scope.newProduct = new Product();
-    // $scope.saveProduct = function () {
-    //   $scope.newProduct.$save(function(product) {
-    //     $scope.products.push(product)
-    //     $scope.newProduct = new Product();
-    //   });
-    // }
-
-    // $scope.deleteProduct = function (product) {
-    //   product.$delete(function() {
-    //     position = $scope.products.indexOf(product);
-    //     $scope.products.splice(position, 1);
-    //   }, function(errors) {
-    //     $scope.errors = errors.data
-    //   });
-    // }
-
-    // $scope.showProduct = function(product) {
-    //   product.details = true;
-    //   product.editing = false;
-    // }
-
-    // $scope.hideProduct = function(product) {
-    //   product.details = false;
-    // }
-
-    // $scope.editProduct = function(product) {
-    //   product.editing = true;
-    //   product.details = false;
-    // }
-
-    // $scope.updateProduct = function(product) {
-    //   product.$update(function() {
-    //     product.editing = false;
-    //   }, function(errors) {
-    //     $scope.errors = errors.data
-    //   });
-    // }
-
-    // $scope.clearErrors = function() {
-    //   $scope.errors = null;
-    // }
 }])
-=======
-    $scope.button = {
-  "toggle": false,
-  "checkbox": {
-    "left": false,
-    "middle": true,
-    "right": true
-  },
-  "radio": 0
-};
-
-
-
-  $scope.items = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
-  $scope.linkItems = {
-    "Google": "http://google.com",
-    "AltaVista": "http://altavista.com"
-  };
-
-
-$scope.alerts = [
-    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
-    { type: 'success round', msg: 'Well done! You successfully read this important alert message.' }
-];
-    $scope.addAlert = function() {
-    $scope.alerts.push({msg: "Another alert!"});
-  };
-
-  $scope.closeAlert = function(index) {
-    $scope.alerts.splice(index, 1);
-  };
-    // $scope.leftAside = {
-    //   "body": "hi"
-    // };
-
-    // $scope.selectedState = "";
-    // $scope.states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
-
-
-    // $scope.popover = {
-    //   "title": "Title",
-    //   "content": "Hello Popover<br />This is a multiline message!"
-    // };
-
-    // $scope.modal = {
-    //   "title": "Title",
-    //   "content": "Hello Modal<br />This is a multiline message!"
-    // };
-
-  
-
-    $scope.saveProduct = function () {
-      $scope.newProduct.$save(function(product) {
-        $scope.products.push(product)
-        $scope.newProduct = new Product();
-      });
-    }
-
-    $scope.deleteProduct = function (product) {
-      product.$delete(function() {
-        position = $scope.products.indexOf(product);
-        $scope.products.splice(position, 1);
-      }, function(errors) {
-        $scope.errors = errors.data
-      });
-    }
-
-    $scope.showProduct = function(product) {
-      product.details = true;
-      product.editing = false;
-    }
-
-    $scope.hideProduct = function(product) {
-      product.details = false;
-    }
-
-    $scope.editProduct = function(product) {
-      product.editing = true;
-      product.details = false;
-    }
-
-    $scope.updateProduct = function(product) {
-      product.$update(function() {
-        product.editing = false;
-      }, function(errors) {
-        $scope.errors = errors.data
-      });
-    }
-
-    $scope.clearErrors = function() {
-      $scope.errors = null;
-    }
-}])
-
->>>>>>> master
