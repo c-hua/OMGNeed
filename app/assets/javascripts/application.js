e687ee454a830bd16a7e0c21e39827b6c3917c9c
Christie Hua <christiehua@gmail.com>
2014-06-05 22:48:14 -0700

    index pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,13 @@
 //= require_tree .
 
 
+   $("[rel='tooltip']").tooltip();    
+ 
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
